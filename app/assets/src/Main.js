c6ec93a3c602378bf12cda808c665c0fcf7cdc04
CIANDT\isaquenogueira <isaquenogueira@ciandt.com>
2023-03-13 13:55:50 -0300

    Added button and textarea

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -1,18 +1,79 @@
-import Core from "./Core.js";
-
 const client = ZAFClient.init();
 let settings;
 
-client.metadata().then((metadata) => {
-  settings = metadata.settings;
-});
+const updateTicketSubject = async () => {
+  const options = {
+    url: "https://teste7094.zendesk.com/api/v2/tickets/update_many?ids=1",
+    type: "POST",
+    contentType: 'application/json',
+    data: JSON.stringify({
+      ticket: {
+        subject: "Test ticket #1",
+        comment: { 
+          body: "This is a test ticket"
+        }
+      }
+    }),
+  };
+  
+  client.request(options).then((response) => {
+    console.log(response);
+  });
+}
 
 const Main = async () => {
   const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+
+  let appBody = `<div id="main-content">
+                    <textarea id="subject_text" name="subject"></textarea>
+                    <button>Click Aqui</button>
+                  </div>`;
 
   // Write App
   App.innerHTML = appBody;
+
+  document.querySelector("button").addEventListener("click", updateTicketSubject);
 };
 
 export default Main;
+
+
+// const client = ZAFClient.init();
+// let settings;
+
+// const updateTicketSubject = async () => {
+//   const options = {
+//     url: "https://teste7094.zendesk.com/api/v2/tickets/update_many?ids=1",
+//     type: "POST",
+//     contentType: 'application/json',
+//     data: JSON.stringify({
+//       ticket: {
+//         subject: "Test ticket #1",
+//         comment: { 
+//           body: "This is a test ticket"
+//         }
+//       }
+//     }),
+//   };
+  
+//   client.request(options).then((response) => {
+//     console.log(response);
+//     document.getElementById("subject_text").value = JSON.stringify(response);
+//   });
+// }
+
+// const Main = async () => {
+//   const App = document.getElementById("app");
+
+//   let appBody = `<div id="main-content">
+//                     <textarea id="subject_text" name="subject"></textarea>
+//                     <button>Click Aqui</button>
+//                   </div>`;
+
+//   // Write App
+//   App.innerHTML = appBody;
+
+//   document.querySelector("button").addEventListener("click", updateTicketSubject);
+// };
+
+// Main();
