901e7ca88c4569fcdbca3133481bd882d9fc059f
CIANDT\isaquenogueira <isaquenogueira@ciandt.com>
2023-03-14 01:10:56 -0300

    Tried to use axios and ZAF unsuccessfully

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -1,79 +1,35 @@
 const client = ZAFClient.init();
 let settings;
 
-const updateTicketSubject = async () => {
-  const options = {
-    url: "https://teste7094.zendesk.com/api/v2/tickets/update_many?ids=1",
-    type: "POST",
-    contentType: 'application/json',
-    data: JSON.stringify({
-      ticket: {
-        subject: "Test ticket #1",
-        comment: { 
-          body: "This is a test ticket"
-        }
+const updateTicketSubject = () => {
+  axios.put({
+    method: 'put',
+    url: 'https://teste7094.zendesk.com/api/v2/tickets/1',
+    data: {
+      "ticket": {
+        "subject": "mudando assunto"
+      },
+      headers: {
+        Authorization: "Basic aXNhcXVlYW4yQGdtYWlsLmNvbTpAMDA3OTM0NTYxY2d0ZnJV"
       }
-    }),
-  };
-  
-  client.request(options).then((response) => {
-    console.log(response);
+    }
   });
 }
 
 const Main = async () => {
   const App = document.getElementById("app");
 
+  // const response = await client.get('ticket.requester.name');
+
   let appBody = `<div id="main-content">
                     <textarea id="subject_text" name="subject"></textarea>
                     <button>Click Aqui</button>
                   </div>`;
 
-  // Write App
   App.innerHTML = appBody;
 
-  document.querySelector("button").addEventListener("click", updateTicketSubject);
+  const button = document.querySelector('button');
+  button.addEventListener('onclick', () => updateTicketSubject)
 };
 
 export default Main;
-
-
-// const client = ZAFClient.init();
-// let settings;
-
-// const updateTicketSubject = async () => {
-//   const options = {
-//     url: "https://teste7094.zendesk.com/api/v2/tickets/update_many?ids=1",
-//     type: "POST",
-//     contentType: 'application/json',
-//     data: JSON.stringify({
-//       ticket: {
-//         subject: "Test ticket #1",
-//         comment: { 
-//           body: "This is a test ticket"
-//         }
-//       }
-//     }),
-//   };
-  
-//   client.request(options).then((response) => {
-//     console.log(response);
-//     document.getElementById("subject_text").value = JSON.stringify(response);
-//   });
-// }
-
-// const Main = async () => {
-//   const App = document.getElementById("app");
-
-//   let appBody = `<div id="main-content">
-//                     <textarea id="subject_text" name="subject"></textarea>
-//                     <button>Click Aqui</button>
-//                   </div>`;
-
-//   // Write App
-//   App.innerHTML = appBody;
-
-//   document.querySelector("button").addEventListener("click", updateTicketSubject);
-// };
-
-// Main();
